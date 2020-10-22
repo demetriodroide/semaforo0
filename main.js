40e47237017d65ba14c9a2aed814b3d12ae1241c
unknown <[EMAIL]>
2020-10-22 16:31:14 +0200

    ahora si

--- main.js
+++ main.js
@@ -1,5 +1,47 @@
+/*
+document.getElementById("idboton").addEventListener("click", escuchoclickboton); 
+//let input = document.getElementById("idcaja");
+
+function escuchoclickboton() { 
+    let input = document.getElementById("idcaja");
+
+        if ( input.value === "" ){
+            document.querySelector("#idcaja").className = "rojo";
+        }
+            else {
+                document.querySelector("#idcaja").className = "verde";
+            }
+}
+*/
+
+/*
+document.getElementById("idboton").addEventListener("click", escuchoclickboton); 
+//let input = document.getElementById("idcaja");
+
+function escuchoclickboton() { 
+    let input = document.getElementById("idcaja");
+
+        if ( input.value.length < 8 ){
+            document.querySelector("#idcaja").className = "rojo";
+        }
+            else {
+                document.querySelector("#idcaja").className = "verde";
+            }
+}
+
+*/
 
 document.getElementById("idboton").addEventListener("click", escuchoclickboton); 
-function escuchoclickboton() { document.querySelector("#idcaja").className = "verde";}
+//let input = document.getElementById("idcaja");
 
+function escuchoclickboton() { 
+    let input = document.getElementById("idcaja");
+
+        if ( input.value.length >= 12 || input.value.length <= 7 || input.value === "" ){
+            document.querySelector("#idcaja").className = "rojo";
+        }
+            else {
+                document.querySelector("#idcaja").className = "verde";
+            }
+}
 
