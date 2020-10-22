80794717f9df524f205d082ebe84f84fb1aa5f56
demetrio <demeuforia@gmail.com>
2020-10-22 19:47:58 +0200

    noseporque

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function escuchoclickboton() {
 }
 
 */
-
+/*
 document.getElementById("idboton").addEventListener("click", escuchoclickboton); 
 //let input = document.getElementById("idcaja");
 
@@ -45,4 +45,48 @@ function escuchoclickboton() {
         document.querySelector("#idcaja").className = "verde";
     }
 }
+*/
+/*
+document.getElementById("idboton").addEventListener("click", escuchoclickboton); 
 
+function escuchoclickboton() { 
+    let input = document.getElementById("idcaja");
+
+        if ( input.value === "" ){
+            document.querySelector("#idcaja").className = "rojo";
+                alert("ERROR, el campo esta vacio")
+        }
+            else {
+                document.querySelector("#idcaja").className = "verde";
+            }
+}
+*/
+
+// input.value.length > 11 || input.value.length < 8 ||
+
+
+
+document.getElementById("idboton").addEventListener("click", escuchoclickboton); 
+//let input = document.getElementById("idcaja");
+
+function escuchoclickboton() { 
+    let input = document.getElementById("idcaja");
+
+    if (  input.value === "" ) {
+        document.querySelector("#idcaja").className = "rojo";
+        alert("ERROR , el campo esta vacio")
+    }
+    else if ( input.value.length < 8 ) {
+        document.querySelector("#idcaja").className = "rojo";
+        alert("ERROR , el campo tiene menos de 8 letras")
+    }
+    else if (  input.value.length < 12 ) {
+        document.querySelector("#idcaja").className = "verde";
+        // document.querySelector("#idcaja").className = "rojo";
+        //alert("ERROR , el campo tiene mas de 12 letras")
+    }
+    else {
+        document.querySelector("#idcaja").className = "rojo";
+        alert("ERROR , el campo tiene mas de 12 letras")
+    }
+}
